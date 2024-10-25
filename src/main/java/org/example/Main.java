2225927fdf5665ff93bba8509b0cbecd6f4d49fb
Illia-Andrieiev <i.e.andreev2005@gmail.com>
2024-10-25 18:09:03 +0300

    improved validation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String xmlDocument = "xml_files/greenhouse.xml";
-        String xmlSchema = "xml_files/greenhouse.xsd";
+        String xmlDocument = "src/main/java/org/example/xml_files/flower.xml";
+        String xmlSchema = "src/main/java/org/example/xml_files/greenhouse.xsd";
         boolean is_valid = XMLValidator.validationXML(xmlDocument,xmlSchema);
         System.out.println(is_valid);
     }
 }
+
+//    <Name> Роза </Name>
+//    <Soil> Підзолистий </Soil>
+//    <Origin> Україна </Origin>
+//    <Multiplying> живцями </Multiplying>
