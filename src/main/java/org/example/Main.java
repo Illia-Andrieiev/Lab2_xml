e01577bf5f75c7454360b06bdc08d4201e422a44
Illia-Andrieiev <i.e.andreev2005@gmail.com>
2024-10-26 22:15:13 +0300

    generated Flower class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
+import generated.Flower;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String xmlDocument = "src/main/java/org/example/xml_files/flower.xml";
+        String xmlDocument = "src/main/java/org/example/xml_files/flowers/f1.xml";
         String xmlSchema = "src/main/java/org/example/xml_files/greenhouse.xsd";
         boolean is_valid = XMLValidator.validationXML(xmlDocument,xmlSchema);
         System.out.println(is_valid);
